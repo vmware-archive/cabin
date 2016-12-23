528cdaf5cb47d65b7e1137ff7df90421455917ea
Jimmy Mayoukou <bhullnatik@gmail.com>
2016-12-24 00:48:55 +0100

    Fix certificate on Android

--- android/app/src/main/java/com/skippbox/cabin/CertificateModule.java
+++ android/app/src/main/java/com/skippbox/cabin/CertificateModule.java
@@ -1,5 +1,7 @@
 package com.skippbox.cabin;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -10,19 +12,19 @@ import com.facebook.react.modules.network.OkHttpClientProvider;
 import com.facebook.react.modules.network.ReactCookieJarContainer;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.KeyStoreBuilderParameters;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
@@ -44,34 +46,31 @@ public class CertificateModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void initClientWithCertificates(ReadableArray clusters, Promise promise) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, KeyManagementException {
-        List<KeyStore.Builder> keys = new ArrayList<>();
-        X509TrustManager trustManager = getTrustManager();
+        List<KeyManager> keys = new ArrayList<>();
+        final X509TrustManager trustManager = getTrustManager();
 
         for (int i = 0; i < clusters.size(); i++) {
             ReadableMap cluster = clusters.getMap(i);
             ReadableMap certificate = cluster.hasKey("certificate") ? cluster.getMap("certificate") : null;
 
-            if (certificate != null && certificate.getString("path") != null) {
+            if (certificate != null && certificate.hasKey("path")) {
+                char[] password = certificate.getString("password").toCharArray();
                 try {
-                    KeyStore.Builder builder = getKeyStoreFromCertificate(certificate);
-                    keys.add(builder);
+                    KeyStore keyStore = KeyStore.getInstance("PKCS12");
+
+                    keyStore.load(new FileInputStream(new File(getReactApplicationContext().getFilesDir() + "/" + certificate.getString("path"))), password);
+                    KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+                    kmf.init(keyStore, password);
+                    keys.addAll(Arrays.asList(kmf.getKeyManagers()));
                 } catch (Exception e) {
+                    Log.e("Cabin", "Error with cluster: " + cluster, e);
                     // wrong passphrase or something, ignore this certificate
                     // TODO: Reject promise with failed clusters ?
                 }
             }
         }
-        KeyManagerFactory kmf;
-
-        try {
-            kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            kmf.init(new KeyStoreBuilderParameters(keys));
-        } catch (Exception e) {
-            // no keys; ignore and don't use it
-            kmf = null;
-        }
         SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(kmf != null ? kmf.getKeyManagers() : null, new TrustManager[]{trustManager}, null);
+        sslContext.init(keys.size() > 0 ? keys.toArray(new KeyManager[keys.size()]) : null, new TrustManager[]{trustManager}, null);
 
         OkHttpClient client = new OkHttpClient.Builder()
                 .connectTimeout(0, TimeUnit.MILLISECONDS)
@@ -83,13 +82,6 @@ public class CertificateModule extends ReactContextBaseJavaModule {
 
         OkHttpClientProvider.replaceOkHttpClient(client);
         promise.resolve(true);
-    }
-
-    private KeyStore.Builder getKeyStoreFromCertificate(ReadableMap certificate) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
-        char[] password = certificate.getString("password").toCharArray();
-        File certificateFile = new File(getReactApplicationContext().getFilesDir() + "/" + certificate.getString("path"));
-
-        return KeyStore.Builder.newInstance("PKCS12",  null, certificateFile, new KeyStore.PasswordProtection(password));
     }
 
     private static X509TrustManager getTrustManager() {
